65c46909833c2844a9fa41c101209623c6447f56
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2018-09-19 19:36:29 -0400

    successful compile

--- cpp2json/test.cpp
+++ cpp2json/test.cpp
@@ -5,7 +5,7 @@ template<typename T>
 T foo(T x, T y) {
 	return x * y;
 }
-//test 2 33 33 d dd ss ff dd ff f3f3 fff dd dd ff ff
+//test 2 33 33 d dd ss ff dd s ff f3f3 fff dd dd ff ff
 int bar(); 
 
 struct Jim {
