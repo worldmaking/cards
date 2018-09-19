019a2a79fcbbb14b664cffe12d4001faa80d9afe
Graham Wakefield <grrrwaaa@gmail.com>
2018-09-19 14:45:41 -0400

    work on projections

--- cpp2json/test.cpp
+++ cpp2json/test.cpp
@@ -1,38 +1,5 @@
 
-
-// multiploy
-template<typename T> 
-T foo(T x, T y) {
-	return x * y;
+// calculate something
+float calc() {
+	return 0;
 }
-
-int bar();
-
-struct Jim {
-
-	struct JJ {
-		int x() { return 0; }
-	};
-	
-	int x = 0;
-	double y;
-
-
-	int getx() { return x; }
-};
-
-Jim jim;
-
-int bar() {
-	jim.x++;
-	return 10;
-}
-
-int main(int argc, char ** argv) {
-	{
-		jim.x = 13;
-	}
-	return foo(jim.getx(), 2);
-}
-
- // test taaaa  sdsd test test rest
