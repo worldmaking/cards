dbce44dd86906e91cbdcd42f4f43b32e80c6216e
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2018-10-03 17:16:10 -0400

    successful compile

--- cpp2json/test.cpp
+++ cpp2json/test.cpp
@@ -5,32 +5,32 @@ template<typename T>
 T foo(T x, T y) {
 	return x * y;
 }
-//test  ss2 33 ssssss sssds ssss sss ssss s ss
+//test  ss2 3 s3 ssss sssssss sssssssds ssss sss ssss s ss
 
 struct Jim {
-
-	struct JJ {
-		int x() { return 0; }
+ 
+	struct JJ { 
+		int x() { return 0; } 
 	};
 	
-	int x = 0;
+	int x = 0; 
 	double y;
+ 
 
-
-	int getx() { return x; }
+	int getx() { return x; }  
 };
-
-Jim jim;
-
-int bar() {
-	jim.x++;
-	return 10;
-}
-
+   
+ Jim jim;  
+  
+int bar() { 
+	jim.x++; 
+	return 10; 
+}   
+  
 int main(int argc, char ** argv) {
 	{
-		jim.x = 13;
-	}
+		jim.x = 13; 
+	} 
 	return foo(jim.getx(), 2);
 }
 
